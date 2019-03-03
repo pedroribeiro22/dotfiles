f97f6deb92fb82fecae87cafec8e56f6d1fab8db
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2019-03-03 11:52:09 +0000

    [haskell]: Set script for Arch

--- haskell/install.sh
+++ haskell/install.sh
@@ -1,10 +1,17 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-echo "Installing Haskell"
-sudo apt-get install ghc 
-audo apt-get install ghc-libs
-sudo apt-get install ghc-static
-sudo apt-get install cabal-install
+# shellcheck source=distro.sh
+. ../distro.sh
+# shellcheck source=helpers.sh
+. ../helpers.sh
 
+echo_info "Installing haskell..."
+_install stack
+_install ghc
+_install ghc-libs
+_install ghc-static
+
+echo_info "Symling .ghci..."
 ln -sfT ~/.dotfiles/haskell/ghci ~/.ghci
 
+echo_done "Haskell configuration!"
