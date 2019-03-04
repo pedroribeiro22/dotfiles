1833829d1cfa0179557a937132fd4bdf7b78b363
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2019-03-04 10:47:01 +0000

    [zsh]: Add 2nd semester aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,7 +7,11 @@ alias c="cd $HOME/Code && ls -l"
 alias see="firefox"
 
 # UNIVERSITY RELATED
-alias um="cd $HOME/Code/uminho/"
+alias poo="cd $HOME/Code/csdegree/2nd year/2nd Semester/Programação Orientada a Objetos"
+alias cp="cd $HOME/Code/csdegree/2nd year/2nd Semester/Cálculo de Programas"
+alias so="cd $HOME/Code/csdegree/2nd year/2nd Semester/Sistemas Operativos"
+alias li="cd $HOME/Code/csdegree/2nd year/2nd Semester/LI3"
+alias eletro="cd $HOME/Code/csdegree/2nd year/2nd Semester/Eletromagnetismo"
 
 # CHANGE KEYBOARD LAYOUT
 alias ptkeyboard="setxkbmap -layout pt"
