4020b1b24bca4ccbdd2a7c07e00e1fb9205f97b5
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2019-07-09 14:08:41 +0100

    :bug: [i3wm]: No need to change keyboard layout

--- i3wm/monitor.sh
+++ i3wm/monitor.sh
@@ -8,15 +8,13 @@ function ActivateHDMI1 {
     echo "Switching to HDMI1"
     xrandr --output HDMI1 --mode 1920x1080 --dpi 160 --output eDP-1 --off
     MONITOR=HDMI1
-    setxkbmap pt
 }
 
 # Function that reacts whenever the HDMI1 cable gets unplugged
 function DeactivateHDMI1 {
     echo "Switching to eDP1"
     xrandr --output HDMI1 --off --output eDP-1 --auto
-    MONITOR=eDP1
-    setxkbmap pt
+    MONITOR=eDP-1
 }
 
 # Function that checks if HDMI1 cable is connected
