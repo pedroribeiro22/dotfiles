116c51e50a4248dfbaa19dc7b90527f6b162240d
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2020-01-21 22:44:54 +0000

    Add wallpaper setting script

---
+++ wallpapers/install.sh
@@ -0,0 +1,7 @@
+echo "Copying wallpaper to the directory ..."
+
+sudo cp Moonlight.jpg /usr/share/backgrounds/i3_default_background.png
+
+yay -S betterlockscreen
+
+betterlockscreen -u Moonlight.jpg
