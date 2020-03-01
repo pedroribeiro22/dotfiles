e4e5968de4a2a22fba540e884030accd36c81ee3
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2020-03-01 11:57:27 +0000

    Change zsh theme to _gallois_

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -3,7 +3,7 @@ export ZSH=~/.oh-my-zsh
 export TERM="xterm-256color"
 
 POWERLEVEL9K_MODE='awesome-fontconfig'
-ZSH_THEME="robbyrussell"
+ZSH_THEME="gallois"
 source $ZSH/oh-my-zsh.sh
 
 # My Powerlevel9k Settings
