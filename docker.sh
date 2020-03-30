d612061c88893762f642d0ec8f772f5836cd02a8
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2020-03-30 16:02:12 +0100

    Update docker script

--- docker.sh
+++ docker.sh
@@ -7,3 +7,6 @@ sudo docker run -p 3306:3306 --name docker-mysql -e MYSQL_ROOT_PASSWORD=dockermy
 
 # MongoDB
 sudo docker run -d -p 27017-27019:27017-27019 --name docker-mongo -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=dockermongo -v /data/mongo:/data/db mongo --auth
+
+sudo docker start docker-mongo
+sudo docker start docker-mysql
