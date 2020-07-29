7f44efd73ffbc1f058e686009145c3c419e1f128
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2020-07-29 02:12:40 +0100

    Update (long overdue)

--- docker.sh
+++ docker.sh
@@ -6,8 +6,6 @@ sudo mkdir -p /data/mysql
 
 sudo docker run -p 3306:3306 --name docker-mysql -e MYSQL_ROOT_PASSWORD=dockermysql --volume=/data/mysql:/var/lib/mysql -d mysql:8.0.18 --secure-file-priv=""
 
-sudo mkdir -p /data/mysql/csv
-
 # MongoDB
 sudo docker run -d -p 27017-27019:27017-27019 --name docker-mongo -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=dockermongo -v /data/mongo:/data/db mongo --auth
 
